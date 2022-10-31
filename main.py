1602942c61b009c6f41be8028d890529237039b6
iskan <iskandergafaroff@gmail.com>
2022-11-01 02:02:10 +0400

    refactoring of 7 task fuck n*****s + add 8 task

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from skimage.io import imshow, show, imread, imsave
 import pywt
+import cv2
+import random
 
 PATH_TO_SOURCE_IMAGE = 'bridge.tif'
 ALPHA = 0.7
-STEP = 0.05
+STEP = 0.02
+MATH_EXPECTATION = 400
+SIGMA = 100
 
 
-def create_watermark(size: int) -> np.ndarray:
-    vector = np.linspace(1, 50, size)
-    mean = np.mean(vector)
-    sd = np.std(vector)
-    prob_density = (np.pi * sd) * np.exp(-0.5 * ((vector - mean) / sd) ** 2)
-    return prob_density
+# def create_watermark(size: int) -> np.ndarray:
+#     vector = np.linspace(1, 50, size)
+#     mean = np.mean(vector)
+#     sd = np.std(vector)
+#     prob_density = (np.pi * sd) * np.exp(-0.5 * ((vector - mean) / sd) ** 2)
+#     return prob_density
+
+
+def create_watermark(length, math_expectation, sigma):
+    result = np.zeros(length)
+    for i in range(0, length, 1):
+        result[i] = random.gauss(math_expectation, sigma)
+    return result
 
 
 def insert_watermark(watermark_values: np.ndarray, place: np.ndarray, alpha: float) -> np.ndarray:
@@ -26,10 +38,9 @@ def detecting(source_watermark_vector, result_watermark_vector):
 
 
 def auto_alpha_selection():
-    result_p = 0.0
-    alpha = 0.0
-    alpha_value = alpha
-    while alpha < 100:
+    result = {}
+    alpha = 0.1
+    while alpha < 1:
         alpha += STEP
         # 5 insert watermark
         ll_place_with_watermark = insert_watermark(watermark, ll_place, alpha)
@@ -46,6 +57,8 @@ def auto_alpha_selection():
         # 8 getting watermark
         ll_place_with_watermark = watermarked_dwt_image[0]
         extracted_watermark = (ll_place_with_watermark - ll_place) / alpha
+        extracted_watermark[extracted_watermark < 0] = 0
+        extracted_watermark[extracted_watermark > 255] = 255
 
         extracted_watermark_vector_length = extracted_watermark.shape[0] * extracted_watermark.shape[1]
         extracted_watermark_vector = extracted_watermark.reshape(extracted_watermark_vector_length)
@@ -53,10 +66,14 @@ def auto_alpha_selection():
         # extracted_watermark_vector = extracted_watermark_vector[0:int(extracted_watermark_vector_length / 2)]
 
         p = detecting(watermark_vector, extracted_watermark_vector)
-        if p > result_p:
-            result_p = p
-            alpha_value = alpha
-    return result_p, alpha_value
+        psnr = cv2.PSNR(watermark, extracted_watermark)
+        if p > 0.9:
+            result[psnr] = alpha
+        # print(f'𝜌: {p}, α: {alpha}, PSNR: {psnr}')
+    min_psnr = min(result.keys())
+    max_alpha = result[min_psnr]
+    print(f'Result: α: {max_alpha}, Min PSNR: {min_psnr}')
+    return max_alpha
 
 
 if __name__ == '__main__':
@@ -75,7 +92,7 @@ if __name__ == '__main__':
     # watermark = np.vstack((create_watermark(watermark_length).reshape(int(ll_place.shape[0] / 2),
     #                                                                       ll_place.shape[1]), zeros))
 
-    watermark_vector = create_watermark(watermark_length)
+    watermark_vector = create_watermark(watermark_length, MATH_EXPECTATION, SIGMA)
     watermark = np.vstack((zeros, watermark_vector.reshape(int(ll_place.shape[0] / 2),
                                                            ll_place.shape[1])))
     print(watermark_vector)
@@ -92,9 +109,12 @@ if __name__ == '__main__':
     watermarked_image = imread('result.tif')
     watermarked_dwt_image = pywt.wavedec2(watermarked_image, wavelet='haar', level=2)
 
-    # 8 getting watermark
+    # 8 (task 6) getting watermark
     ll_place_with_watermark = watermarked_dwt_image[0]
     extracted_watermark = (ll_place_with_watermark - ll_place) / ALPHA
+    extracted_watermark[extracted_watermark < 0] = 0
+    extracted_watermark[extracted_watermark > 255] = 255
+
 
     extracted_watermark_vector_length = extracted_watermark.shape[0] * extracted_watermark.shape[1]
     extracted_watermark_vector = extracted_watermark.reshape(extracted_watermark_vector_length)
@@ -102,7 +122,33 @@ if __name__ == '__main__':
     # extracted_watermark_vector = extracted_watermark_vector[0:int(extracted_watermark_vector_length / 2)]
 
     p = detecting(watermark_vector, extracted_watermark_vector)
-
-    print(auto_alpha_selection())
-
     print(p)
+    # task 7
+    # print(auto_alpha_selection())
+
+    p_arr = []
+    for i in range(100):
+        watermark_vector = create_watermark(watermark_length, MATH_EXPECTATION, SIGMA)
+        watermark = np.vstack((zeros, watermark_vector.reshape(int(ll_place.shape[0] / 2),
+                                                               ll_place.shape[1])))
+        ll_place_with_watermark = insert_watermark(watermark, ll_place, ALPHA)
+        dwt_image[0] = ll_place_with_watermark
+        invert_dwt_image = pywt.waverec2(dwt_image, wavelet='haar').astype(np.uint8)
+        imsave('result.tif', invert_dwt_image)
+        watermarked_image = imread('result.tif')
+        watermarked_dwt_image = pywt.wavedec2(watermarked_image, wavelet='haar', level=2)
+        ll_place_with_watermark = watermarked_dwt_image[0]
+        extracted_watermark = (ll_place_with_watermark - ll_place) / ALPHA
+        extracted_watermark[extracted_watermark < 0] = 0
+        extracted_watermark[extracted_watermark > 255] = 255
+        extracted_watermark_vector_length = extracted_watermark.shape[0] * extracted_watermark.shape[1]
+        extracted_watermark_vector = extracted_watermark.reshape(extracted_watermark_vector_length)
+        extracted_watermark_vector = extracted_watermark_vector[int(extracted_watermark_vector_length / 2):]
+        # extracted_watermark_vector = extracted_watermark_vector[0:int(extracted_watermark_vector_length / 2)]
+
+        p_arr.append(detecting(watermark_vector, extracted_watermark_vector))
+    print(p_arr)
+    distance = np.arange(0, 100)
+    plt.plot(distance, p_arr, '-o')
+    plt.show()
+    # print(p)
